e83373b7b348f5248744e6350d6031bded344139
Ariel Ril <arielril@gmail.com>
2019-08-11 00:58:08 -0300

    add linter add random sampler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+import rnd from "./random";
+
+const MAX = 5000;
+
+const RND = rnd(MAX, 47, 23, Date.now())
+
+console.log('# X', 'Y')
+for (let i = 0; i < 1000; i += 1)
+  console.log(i, RND() / MAX)
